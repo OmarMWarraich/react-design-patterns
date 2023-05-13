36436eca5c9e740e44ad45e59af0b5ca7087ef36
OmarMWarraich <earthling986@gmail.com>
2023-05-13 09:03:59 +0500

    feat: Lists and Types

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import { SplitScreen } from "./SplitScreen";
+import { RegularList } from "./RegularList";
+import { SmallPersonListItem } from "./people/smallPersonListItem";
+import { LargePersonListItem } from "./people/largePersonListItem";
+import { SmallProductListItem } from "./products/smallProductsListItem";
+import { LargeProductListItem } from "./products/largeProductsListItem";
 
-const LeftHandComponent = ({ name }) => {
-  return (
-    <h1 style={{backgroundColor:"green"}}>
-      {name || "Left!"}
-    </h1>
-  );
-}
+const people = [{
+	name: 'John Doe',
+	age: 54,
+	hairColor: 'brown',
+	hobbies: ['swimming', 'bicycling', 'video games'],
+}, {
+	name: 'Brenda Smith',
+	age: 33,
+	hairColor: 'black',
+	hobbies: ['golf', 'mathematics'],
+}, {
+	name: 'Jane Garcia',
+	age: 27,
+	hairColor: 'blonde',
+	hobbies: ['biology', 'medicine', 'gymnastics'],
+}];
 
-const RightHandComponent = ({ message }) => {
-  return (
-    <h1 style={{backgroundColor:"red"}}>
-      {message || "Right!"}
-    </h1>
-  );
-}
+const products = [{
+	name: 'Flat-Screen TV',
+	price: '$300',
+	description: 'Huge LCD screen, a great deal',
+	rating: 4.5,
+}, {
+	name: 'Basketball',
+	price: '$10',
+	description: 'Just like the pros use',
+	rating: 3.8,
+}, {
+	name: 'Running Shoes',
+	price: '$120',
+	description: 'State-of-the-art technology for optimum running',
+	rating: 4.2,
+}];
 
 function App() {
-  return (
-    <SplitScreen leftWeight={3} rightWeight={1}>
-      <LeftHandComponent name="Omar"/>
-      <RightHandComponent message="hello"/>
-    </SplitScreen>
-  );
+	return (
+    <>
+		  <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem}
+      />
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={SmallProductListItem}
+      />
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={LargeProductListItem}
+      />
+    </>
+	);
 }
 
 export default App;
