1d57724f436c594a22f4c38240df86e7a01fc5c8
OmarMWarraich <earthling986@gmail.com>
2023-05-14 10:59:11 +0500

    feat: ResourceLoader Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 // import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserLoader } from './UserLoader';
+// import { UserLoader } from './UserLoader';
+import { ResourceLoader } from './ResourceLoader';
 import { UserInfo } from './UserInfo';
+import { ProductInfo } from './ProductInfo';
 
 function App() {
 	return (
 		<>
-		<UserLoader userId="123">
-			<UserInfo />
-		</UserLoader>
-		<UserLoader userId="234">
-			<UserInfo />
-		</UserLoader>
-		<UserLoader userId="345">
-			<UserInfo />
-		</UserLoader>
+			<ResourceLoader resourceUrl="/users/123" resourceName="user">
+				<UserInfo />
+			</ResourceLoader>
+			<ResourceLoader resourceUrl="/products/1234" resourceName="product">
+				<ProductInfo />
+			</ResourceLoader>
 		</>
 	);
 }
