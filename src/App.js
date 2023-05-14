39e40b4827b8cd382737e52dcda3e355518fc58b
OmarMWarraich <earthling986@gmail.com>
2023-05-14 12:45:07 +0500

    feat: Uncontrolled Form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import axios from 'axios';
-import { DataSource } from './DataSource';
-import { UserInfo } from './UserInfo';
-
-const getServerData = url => async () => {
-	const response = await axios.get(url);
-	return response.data;
-}
-
-const getLocalStorageData = key => () => {
-	return localStorage.getItem(key);
-}
-
-const Text = ({ message }) => <h1>{message}</h1>;
+import { UncontrolledForm } from './UncontrolledForm';
 
 function App() {
 	return (
-		<>
-		<DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
-			<UserInfo />
-		</DataSource>
-		<DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
-			<Text />
-		</DataSource>
-		</>
+		<UncontrolledForm />
 	);
 }
 
